f210bb5a522c43262bbd7f5d84c8dd6a66b02186
Igor Zosimovich <igor.zosimovich@gmail.com>
2018-08-27 22:10:34 +0500

    Добавьте файлы проекта.

---
+++ RabbitAdapter/RabbitAdapter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace RabbitAdapter
+{
+    [Guid("DE08E197-26FA-4A0B-8542-08E7D336BC9E")]
+
+    internal interface IRabbitAdapter
+
+    {
+
+        [DispId(1)]
+
+        void message(string msg);
+
+    }
+
+
+
+    [Guid("80B67123-1C81-45F1-9B98-B233C0C6DFBE"), InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
+
+    public interface IRabbitAdapterEvents
+
+    {
+
+    }
+
+
+
+    [Guid("BEB7F94E-522F-488C-9927-8395AFDF51AC"), ClassInterface(ClassInterfaceType.None), ComSourceInterfaces(typeof(IRabbitAdapterEvents))]
+
+    public class RabbitAdapter : IRabbitAdapter
+
+    {
+
+        public void message(string msg)
+
+        {
+
+            MessageBox.Show(msg, "Cообщение компоненты RabbitAdapter", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+        }
+
+    }
+}
