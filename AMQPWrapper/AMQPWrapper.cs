124c8f93366d11776c3595662eeff7d8c33ae343
Igor Zosimovich <igor.zosimovich@gmail.com>
2018-10-07 20:31:31 +0500

    Made it run...

--- AMQPWrapper/AMQPWrapper.cs
+++ AMQPWrapper/AMQPWrapper.cs
@@ -3,134 +3,159 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using RabbitMQ.Client;
 using RabbitMQ.Client.MessagePatterns;
 using RabbitMQ.Client.Events;
+using OneC.ExternalComponents;
 
-namespace RabbitAdapter
+[Guid("7166906E-EAC8-43D1-9B32-4FFE8FDAEEC3")]
+[ProgId("AddIn.AMQPWrapper")]
+public class AMQPWrapper : ExtComponentBase
 {
-    [Guid("DE08E197-26FA-4A0B-8542-08E7D336BC9E")]
+    private int MyNumber;
+    IConnection connection;
+    IModel model;
 
-    internal interface IRabbitAdapter
-
+    public AMQPWrapper()
     {
-
-        [DispId(1)]
-
-        void message(string msg);
-
-        [DispId(2)]
-
-        void InitializeRabbit();
-
-        [DispId(3)]
-
-        void SendToExchange(string ExchangeName, string Message);
-               
+        ComponentName = "AMQPWrapper";
+        //MessageBox.Show("constructor", "constructor procedure", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        //InitEvent += new InitEventHandler(Initialization);
     }
 
-
-
-    [Guid("80B67123-1C81-45F1-9B98-B233C0C6DFBE"), InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
-
-    public interface IRabbitAdapterEvents
-
+    ~AMQPWrapper()
     {
-
+        Disconnect();
     }
 
-
-
-    [Guid("BEB7F94E-522F-488C-9927-8395AFDF51AC"), ClassInterface(ClassInterfaceType.None), ComSourceInterfaces(typeof(IRabbitAdapterEvents))]
-
-    public class AMQPWrapper : IRabbitAdapter
-
+    [Export1c]
+    public void DoSomething(string msg)
     {
+        //            MessageBox.Show(msg, "Cообщение компоненты RabbitAdapter", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        int num = 0;
+        TimerCallback tm = new TimerCallback(Count);
+        // создаем таймер
+        System.Threading.Timer timer = new System.Threading.Timer(tm, num, 0, 2000);
 
-        string exchangeName = "test";
-        string queueName = "test";
-        string routingKey = "test";
+        MessageBox.Show(msg, "Cообщение компоненты RabbitAdapter", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+    }
 
-        IConnection connection;
-        
-        public AMQPWrapper()
+    public void Count(object obj)
+    {
+        Async.ExternalEvent("AddIn.AMQPWrapper", "SampleEvent", "Hi from external component! " + MyNumber.ToString());
+        MyNumber++;
+    }
+
+    [Export1c]
+    public bool Connect(string host, string port, string login, string password, string vhost)
+    {
+        //MessageBox.Show("connect method call", "Cообщение компоненты RabbitAdapter", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+        connection = GetRabbitConnection(host, port, login, password, vhost);
+
+        return true;
+    }
+
+    [Export1c]
+    public void Disconnect()
+    {
+        if (model != null)
         {
-            
+            model.Close();
+            model = null;
         }
-        
-        public void message(string msg)
+        if (connection != null)
         {
-            MessageBox.Show(msg, "Cообщение компоненты RabbitAdapter!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            connection.Close();
+            connection = null;
         }
+    }
 
-        public void InitializeRabbit()
+    [Export1c]
+    public bool Send(string exchangeName, string queueName, string routingKey, string Message)
+    {
+        SendMessage(exchangeName, routingKey, Message);
+
+        return true;
+    }
+
+    [Export1c]
+    public string Receive(string exchangeName, string queueName, string routingKey)
+    {
+        return ReceiveIndividualMessage(exchangeName, queueName, routingKey);
+    }
+
+    #region "rabbit interface"
+
+    private IConnection GetRabbitConnection(string host, string port, string login, string password, string vhost)
+    {
+        ConnectionFactory factory = new ConnectionFactory
         {
-        
-            connection = GetRabbitConnection();
+            UserName = login,
+            Password = password,
+            VirtualHost = vhost,
+            HostName = host + port
+        };
+        IConnection conn = factory.CreateConnection();
+        return conn;
+    }
 
+    private void GetRabbitChannel()
+    {
+        if (model == null)
+        {
+            model = connection.CreateModel();
         }
+//        model.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+//        model.QueueDeclare(queueName, false, false, false, null);
+//        model.QueueBind(queueName, exchangeName, routingKey, null);
+    }
 
-        public void DeinitializeRabbit(){
-        }
-
-        public void SendToExchange(string ExchangeName, string Message)
+    private void DeclareExchange(string exchangeName)
+    {
+        if (model != null)
         {
-
-
-
-        }
-
-        public string GetFromQueue()
-        {
-            return "";
-        }
-        
-        private IConnection GetRabbitConnection()
-        {
-            ConnectionFactory factory = new ConnectionFactory
-            {
-                UserName = "guest",
-                Password = "guest",
-                VirtualHost = "/",
-                HostName = "localhost"
-            };
-            IConnection conn = factory.CreateConnection();
-            return conn;
-        }        
-
-        private IModel GetRabbitChannel(string exchangeName, string queueName, string routingKey)
-        {
-            IModel model = connection.CreateModel();
             model.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+        }
+    }
+
+    private void BindQueue(string exchangeName, string queueName, string routingKey)
+    {
+        if (model != null)
+        {
             model.QueueDeclare(queueName, false, false, false, null);
             model.QueueBind(queueName, exchangeName, routingKey, null);
-            return model;
-        }
-
-        private void SendMessage()
-        {
-            IModel model = GetRabbitChannel(exchangeName, queueName, routingKey);
-            byte[] messageBodyBytes = Encoding.UTF8.GetBytes("Hello, world!");
-            model.BasicPublish(exchangeName, routingKey, null, messageBodyBytes);
-        }
-
-        private string ReceiveIndividualMessage()
-        {
-            string originalMessage = "";
-            IModel model = GetRabbitChannel(exchangeName, queueName, routingKey);
-            BasicGetResult result = model.BasicGet(queueName, false);
-            if (result == null)
-            {
-                // В настоящее время нет доступных сообщений.
-            }
-            else
-            {
-                byte[] body = result.Body;
-                originalMessage = Encoding.UTF8.GetString(body);
-            }
-            return originalMessage;
         }
     }
+
+    private void SendMessage(string exchangeName, string routingKey, string Message)
+    {
+        GetRabbitChannel();
+        DeclareExchange(exchangeName);
+        byte[] messageBodyBytes = Encoding.UTF8.GetBytes(Message);
+        model.BasicPublish(exchangeName, routingKey, null, messageBodyBytes);
+    }
+
+    private string ReceiveIndividualMessage(string exchangeName, string queueName, string routingKey)
+    {
+        string originalMessage = "";
+        GetRabbitChannel();
+        BindQueue(exchangeName, queueName, routingKey);
+        BasicGetResult result = model.BasicGet(queueName, true);
+        if (result == null)
+        {
+            // В настоящее время нет доступных сообщений.
+        }
+        else
+        {
+            byte[] body = result.Body;
+            originalMessage = Encoding.UTF8.GetString(body);
+        }
+        return originalMessage;
+    }
+
+    #endregion
 }
